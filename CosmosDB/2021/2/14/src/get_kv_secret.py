ec25a1e1e1217b33ee45730b5510bffd830ef3d3
まっぴぃ⊿ <46888598+ymasaoka@users.noreply.github.com>
2021-02-13 18:57:30 +0900

    Update get_kv_secret.py

--- CosmosDB/2021/2/14/src/get_kv_secret.py
+++ CosmosDB/2021/2/14/src/get_kv_secret.py
@@ -9,13 +9,13 @@ os.environ['AZURE_CLIENT_ID'] = '' # appId
 os.environ['AZURE_CLIENT_SECRET'] = '' # password
 os.environ['AZURE_TENANT_ID'] = '' # tenant
 
-kvName = os.environ['KEY_VAULT_NAME']
-kvEndpoint = f"https://{kvName}.vault.azure.net"
+kv_name = os.environ['KEY_VAULT_NAME']
+kv_endpoint = f"https://{kv_name}.vault.azure.net"
 kvSecName = os.environ['SECRET_NAME_COSMOS']
 
 # Create Azure Credential
 cred = DefaultAzureCredential()
-secret_client = SecretClient(vault_url=kvEndpoint,credential=cred)
+secret_client = SecretClient(vault_url=kv_endpoint,credential=cred)
 
 # Get Azure Key Vault Secret
 retrieved_secret = secret_client.get_secret(kvSecName)
